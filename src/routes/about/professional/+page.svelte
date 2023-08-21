a31c7b453e725b8185352c71fb298d4dfc87b93f
Dale Bendixen <dalebendixen@gmail.com>
2023-08-21 14:21:03 -0400

    set up layout for professional career section

--- src/routes/about/professional/+page.svelte
+++ src/routes/about/professional/+page.svelte
@@ -1,1 +1,68 @@
-<h1>My Professional Page</h1>
+<div class="container">
+	<div>
+		<h1 class="title">Early Career:</h1>
+	</div>
+	<div>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sint tenetur similique, illo
+			obcaecati unde deleniti asperiores quidem ipsam vero nesciunt id officia, molestias ea vitae
+			nihil. Nam vel est corporis obcaecati maxime nesciunt. Itaque minima ea neque nemo corrupti
+			quasi cupiditate ab ex explicabo consequuntur eum ullam unde tempore qui repudiandae labore,
+			perspiciatis assumenda eveniet, nam voluptatum, excepturi eius! Dolorem animi laborum ipsum
+			laudantium, voluptas illo placeat, aperiam aut eaque velit unde deleniti eius at
+			exercitationem quos corrupti iusto fuga atque nulla numquam voluptate. Magni laudantium
+			deleniti molestias non repudiandae officiis cupiditate a consequatur id hic, mollitia magnam
+			similique.
+		</p>
+	</div>
+</div>
+<div class="container">
+	<div>
+		<h1 class="title">Middle Career:</h1>
+	</div>
+	<div>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sint tenetur similique, illo
+			obcaecati unde deleniti asperiores quidem ipsam vero nesciunt id officia, molestias ea vitae
+			nihil. Nam vel est corporis obcaecati maxime nesciunt. Itaque minima ea neque nemo corrupti
+			quasi cupiditate ab ex explicabo consequuntur eum ullam unde tempore qui repudiandae labore,
+			perspiciatis assumenda eveniet, nam voluptatum, excepturi eius! Dolorem animi laborum ipsum
+			laudantium, voluptas illo placeat, aperiam aut eaque velit unde deleniti eius at
+			exercitationem quos corrupti iusto fuga atque nulla numquam voluptate. Magni laudantium
+			deleniti molestias non repudiandae officiis cupiditate a consequatur id hic, mollitia magnam
+			similique.
+		</p>
+	</div>
+</div>
+<div class="container">
+	<div>
+		<h1 class="title">Present Day:</h1>
+	</div>
+	<div>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sint tenetur similique, illo
+			obcaecati unde deleniti asperiores quidem ipsam vero nesciunt id officia, molestias ea vitae
+			nihil. Nam vel est corporis obcaecati maxime nesciunt. Itaque minima ea neque nemo corrupti
+			quasi cupiditate ab ex explicabo consequuntur eum ullam unde tempore qui repudiandae labore,
+			perspiciatis assumenda eveniet, nam voluptatum, excepturi eius! Dolorem animi laborum ipsum
+			laudantium, voluptas illo placeat, aperiam aut eaque velit unde deleniti eius at
+			exercitationem quos corrupti iusto fuga atque nulla numquam voluptate. Magni laudantium
+			deleniti molestias non repudiandae officiis cupiditate a consequatur id hic, mollitia magnam
+			similique.
+		</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		margin: 4em auto 2em auto;
+		border: 1px solid #dc400f;
+		border-radius: 1em;
+		padding: 1em;
+		width: 90dvw;
+	}
+	.title {
+		text-decoration: underline;
+		margin-bottom: 0.2em;
+	}
+</style>
