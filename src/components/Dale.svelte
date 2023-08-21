8b776c858c22ee50f2c903d0fbd6ce7029846cbf
Dale Bendixen <dalebendixen@gmail.com>
2023-08-20 20:57:50 -0400

    Initial set up, created enter button and made responsive

---
+++ src/components/Dale.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { fly } from 'svelte/transition';
+</script>
+
+<div class="Dale">
+	<h1>Dale</h1>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<div class="enter-btn">
+	<a href="/home">Enter Site</a>
+</div>
+
+<style>
+	.enter-btn {
+		display: grid;
+		border: 1px solid blue;
+		height: 100dvh;
+		text-align: center;
+		align-items: center;
+	}
+	a {
+		margin: 0 auto;
+		background-image: linear-gradient(to right, rgba(166, 52, 19, 0), rgba(166, 52, 19, 0.2));
+		border: 2px solid red;
+		border-radius: 0.1em;
+		width: 10dvw;
+		text-decoration: none;
+		color: #dc400f;
+		font-weight: bold;
+		letter-spacing: 0.5rem;
+		font-size: 2em;
+		cursor: pointer;
+	}
+
+	a:hover {
+		background-color: #dc400f;
+		border: 2px solid #dc400f;
+		color: antiquewhite;
+	}
+
+	@media screen and (max-width: 480px) {
+		a {
+			width: 40dvw;
+		}
+	}
+	@media screen and (min-width: 481px) {
+		a {
+			width: 20dvw;
+		}
+	}
+	@media screen and (min-width: 1199px) {
+		a {
+			width: 10dvw;
+		}
+	}
+</style>
